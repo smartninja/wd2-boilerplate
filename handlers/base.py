dc9d6b890880f66e4d2e8563807b870804cb41de
Matej Ramuta <matej.ramuta@gmail.com>
2016-11-30 14:49:33 +0100

    User get or create

--- handlers/base.py
+++ handlers/base.py
@@ -2,6 +2,7 @@ import os
 import jinja2
 import webapp2
 from google.appengine.api import users
+from models.user import User
 
 template_dir = os.path.join(os.path.dirname(__file__), "../templates")
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
@@ -29,7 +30,7 @@ class BaseHandler(webapp2.RequestHandler):
 
         user = users.get_current_user()
         if user:
-            params["user"] = user
+            params["user"] = User.get_or_create(user.email())
             params["logout_url"] = users.create_logout_url('/')
         else:
             params["login_url"] = users.create_login_url('/')
--- models/user.py
+++ models/user.py
@@ -8,3 +8,14 @@ class User(ndb.Model):
     admin = ndb.BooleanProperty(default=False)
     created = ndb.DateTimeProperty(auto_now_add=True)
     updated = ndb.DateTimeProperty(auto_now=True)
+
+    @classmethod
+    def get_or_create(cls, email):
+        user = User.query(User.email == email).get()  # check if user already exists in the database
+
+        if not user:
+            # if user does not exist yet, create a new one
+            user = User(email=email)
+            user.put()
+
+        return user
