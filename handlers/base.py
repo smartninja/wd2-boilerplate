00a45d56407f888c8a89deb567aa4964f47f168e
Matej Ramuta <matej.ramuta@gmail.com>
2016-11-30 14:44:02 +0100

    login and logout

--- handlers/base.py
+++ handlers/base.py
@@ -1,6 +1,7 @@
 import os
 import jinja2
 import webapp2
+from google.appengine.api import users
 
 template_dir = os.path.join(os.path.dirname(__file__), "../templates")
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
@@ -25,6 +26,13 @@ class BaseHandler(webapp2.RequestHandler):
         cookie_law = self.request.cookies.get("cookie_law")
         if cookie_law:
             params["cookies"] = True
+
+        user = users.get_current_user()
+        if user:
+            params["user"] = user
+            params["logout_url"] = users.create_logout_url('/')
+        else:
+            params["login_url"] = users.create_login_url('/')
 
         template = jinja_env.get_template(view_filename)
         return self.response.out.write(template.render(params))
