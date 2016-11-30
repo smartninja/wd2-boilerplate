c49838a8ea8e825619cb4ae06c9d8331151e9ff6
Matej Ramuta <matej.ramuta@gmail.com>
2016-11-30 18:10:23 +0100

    topic_title added to Comment

--- models/comment.py
+++ models/comment.py
@@ -5,5 +5,6 @@ class Comment(ndb.Model):
     content = ndb.TextProperty()
     author_email = ndb.StringProperty()
     topic_id = ndb.IntegerProperty()
+    topic_title = ndb.StringProperty()
     created = ndb.DateTimeProperty(auto_now_add=True)
     deleted = ndb.BooleanProperty(default=False)
