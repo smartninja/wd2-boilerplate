6885d1b53df55b0bb641c6d62dcf9addcecae628
Matej Ramuta <matej.ramuta@gmail.com>
2016-12-10 16:27:44 +0100

    updated field

--- models/comment.py
+++ models/comment.py
@@ -8,6 +8,7 @@ class Comment(ndb.Model):
     topic_id = ndb.IntegerProperty()
     topic_title = ndb.StringProperty()
     created = ndb.DateTimeProperty(auto_now_add=True)
+    updated = ndb.DateTimeProperty(auto_now=True)
     deleted = ndb.BooleanProperty(default=False)
 
     @classmethod
