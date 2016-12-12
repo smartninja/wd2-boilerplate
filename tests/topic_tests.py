78abd058bc8ad38e5b4192946115cd8268114edb
Matej Ramuta <matej.ramuta@gmail.com>
2016-12-12 15:07:32 +0100

    homework for chapter 10

--- tests/topic_tests.py
+++ tests/topic_tests.py
@@ -6,7 +6,8 @@ import webtest
 
 from google.appengine.ext import testbed
 from google.appengine.api import memcache
-from handlers.topics import TopicAdd, TopicDetails
+from handlers.base import MainHandler
+from handlers.topics import TopicAdd, TopicDetails, TopicDelete
 from models.topic import Topic
 
 
@@ -16,6 +17,8 @@ class TopicTests(unittest.TestCase):
             [
                 webapp2.Route('/topic/add', TopicAdd, name="topic-add"),
                 webapp2.Route('/topic/<topic_id:\d+>', TopicDetails, name="topic-details"),
+                webapp2.Route('/', MainHandler, name="main-page"),
+                webapp2.Route('/topic/<topic_id:\d+>/delete', TopicDelete, name="topic-delete"),
             ])
 
         self.testapp = webtest.TestApp(app)
@@ -57,3 +60,30 @@ class TopicTests(unittest.TestCase):
         topic = Topic.query().get()  # get the topic create by this text (it's the only one in this fake database)
         self.assertTrue(topic.title, title)  # check if topic title is the same as we wrote above
         self.assertTrue(topic.content, text)
+
+    def test_topic_details_handler(self):
+        # GET
+        topic = Topic(title="Another topic", content="Some text in the topic", author_email="some.user@example.com")
+        topic.put()
+
+        get = self.testapp.get('/topic/{}'.format(topic.key.id()))  # do a GET request
+        self.assertEqual(get.status_int, 200)
+
+    def test_topic_delete_handler(self):
+        # POST
+        topic = Topic(title="Topic to delete", content="Some text in the topic", author_email="some.user@example.com")
+        topic.put()  # save topic in database
+
+        topic_query_1 = Topic.query().get()
+        self.assertTrue(topic_query_1)  # assert that topic exists in a database
+
+        csrf_token = str(uuid.uuid4())  # create a CSRF token
+        memcache.add(key=csrf_token, value=True, time=600)  # save token to memcache
+
+        params = {"csrf_token": csrf_token}
+
+        post = self.testapp.post('/topic/{}/delete'.format(topic.key.id()), params)  # do a GET request
+        self.assertEqual(post.status_int, 302)  # when topic is deleted, handler redirects to main page (302 == redirect)
+
+        topic_query_2 = Topic.query().get()
+        self.assertTrue(topic_query_2.deleted)  # assert that "deleted" field is set to True
